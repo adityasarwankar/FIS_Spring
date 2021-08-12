09ee22c3b27bd5816c63f93c9d2e021339b0a674
Aditya Sarwankar <addysarwankar@gmail.com>
2021-08-12 15:35:00 +0530

    iNTEGRATED JPA & SECURITY

---
+++ spring-security/src/main/java/com/fis/springsecurity/product/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.fis.springsecurity.product;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
+}
